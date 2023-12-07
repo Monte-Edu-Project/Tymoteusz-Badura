a06af93b3f3442d36b064255a64b49d5b7aefe13
Tymogekh <74155954+Tymogekh@users.noreply.github.com>
2023-12-07 18:17:09 +0100

    Added java script basics...

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,35 @@
-myButton = function(buttonName){
-    return document.getElementById(buttonName)
+myButton = function(idName){
+    return document.querySelector(idName)
 };
 
-buttonDirect = function(){
-    window.location.href = "C:/Users/uczen/Documents/GitHub/Tymoteusz-Badura/lessons.html";
-}
+lessonDirect = function(){
+    window.location.href = "lessons.html";
+};
+
+indexDirect = function(){
+    window.location.href = "index.html";
+};
+
+preferenceDirect = function(){
+    window.location.href = "preferences.html";
+};
+
+let mainButtonColor = "#FFEF85";
+
+let mainButtonHighlightColor = "#E6C700";
+
+let secondaryButtonColor = "#A2DFFF";
+
+let secondaryButtonHighlightColor = "#1AAFFF";
+
+let closeButtonColor = "#333333";
+
+let closeButtonHighlightColor = "#1A1A1A";
+
+buttonHighlight = function(idName, colorId){
+    myButton("#"+idName).style.backgroundColor = colorId;
+};
+
+buttonReset = function(idName, colorId){
+    myButton("#"+idName).style.backgroundColor = colorId;
+};
