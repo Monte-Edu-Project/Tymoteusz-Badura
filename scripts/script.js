2d4775824cea325153dcb7d1e345c29c7060beab
Tymoteusz <74155954+Tymogekh@users.noreply.github.com>
2023-12-07 10:08:38 +0100

    Also progress...

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+myButton = function(buttonName){
+    return document.getElementById(buttonName)
+};
+
+buttonDirect = function(){
+    window.location.href = "C:/Users/uczen/Documents/GitHub/Tymoteusz-Badura/lessons.html";
+}
