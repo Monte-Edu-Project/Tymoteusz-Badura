e0c754da9a38c0990ea4800922e0b78ff1556d4c
Tymoteusz <74155954+Tymogekh@users.noreply.github.com>
2023-12-14 08:35:04 +0100

    let to const!

--- scripts/script.js
+++ scripts/script.js
@@ -14,17 +14,17 @@ preferenceDirect = function(){
     window.location.href = "preferences.html";
 };
 
-let mainButtonColor = "#FFEF85";
+const mainButtonColor = "#FFEF85";
 
-let mainButtonHighlightColor = "#E6C700";
+const mainButtonHighlightColor = "#E6C700";
 
-let secondaryButtonColor = "#A2DFFF";
+const secondaryButtonColor = "#A2DFFF";
 
-let secondaryButtonHighlightColor = "#1AAFFF";
+const secondaryButtonHighlightColor = "#1AAFFF";
 
-let closeButtonColor = "#333333";
+const closeButtonColor = "#333333";
 
-let closeButtonHighlightColor = "#1A1A1A";
+const closeButtonHighlightColor = "#1A1A1A";
 
 buttonHighlight = function(idName, colorId){
     myButton("#"+idName).style.backgroundColor = colorId;
